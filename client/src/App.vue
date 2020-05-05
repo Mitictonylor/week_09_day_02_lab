7e5b7d601e05642e800ecb8ad8a3f69882dcc1dc
Craig Barratt <craigtbarratt@gmail.com>
2020-05-05 15:07:04 +0100

    adds basic setup to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,48 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <p class="title">Hotel Bookings</p>
+    <booking-form></booking-form>
+    <booking-list :bookings="bookings"><booking-list>
   </div>
 </template>
 
 <script>
+import BookingForm from '@/components/BookingForm.vue';
+import BookingList from '@/components/BookingList.vue';
 
 export default {
   name: 'App',
   data(){
     return{
-      bookingList: [],
-      bookingApi:[]
-      newBooking:null
+      bookings: [],
+      newBooking: null
     }
   },
-  methods:{
-    addInBookingList:(){}
-  }
   components: {
-
-
+    "booking-form": BookingForm,
+    "booking-list": BookingList,
+  },
+  mounted() {
+    this.getBookings()
+  },
+  methods: {
+    getBookings(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings)
+    }
   }
 }
 </script>
 
+<style lang="css" scoped>
+.title {
+  font-size: 100px;
+}
+</style>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+p {
+  padding: 0;
+  margin: 0;
 }
 </style>
